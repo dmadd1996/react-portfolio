47e70edd40ef1171a63c1094b7a2c618cdce1d38
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-07-25 15:54:56 -0500

    added components and content from app_showcase project

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <section id="about" className="hide-small hide-medium">
+            <header className="h1 center padding-64">
+                ABOUT ME
+            </header>
+            <p className="h2 center padding-32 padding-center">I am the IT administrator at BridgeWay To Independence Inc. and I hold a Bachelor's of Applied Science from the Univerity of Minnesota. lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <p className="h2 center padding-32 padding-center">I have been working on web development for a few years with a focus on front end web design. lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </section>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+function Header() {
+    return (
+        <nav className="sidebar bar bar-block small hide-small center">
+            <img src="./Assets/images/avatar_resize.jpg" alt="small boye"  />
+
+                {/* <a href="#" className="bar-item button padding-large black">
+                    <i className="fa-solid fa-house large"></i>
+                    <p className="body">HOME</p>
+                </a>
+                <a href="#about" className="bar-item button padding-large hover-black">
+                    <i className="fa fa-user large"></i>
+                    <p className="body">ABOUT</p>
+                </a>
+                <a href="#apps" className="bar-item button padding-large hover-black">
+                    <i className="fa-solid fa-bars-progress large"></i>
+                    <p className="body">APPS</p>
+                </a>
+                <a href="#contact" className="bar-item button padding-large hover-black">
+                    <i className="fa fa-envelope large"></i>
+                    <p className="body">CONTACT</p>
+                </a> */}
+        </nav>
+
+    );
+}
+
+export default Header;
